ced238ae16dd28426d9f3927567e475d9449c00d
gregoiremahon <greg.mahondu78@gmail.com>
2023-06-08 13:52:51 +0200

    Initial Commit

---
+++ MojiViewer/MojiViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MojiViewerApp.swift
+//  MojiViewer
+//
+//  Created by Grégoire Mahon on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MojiViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
